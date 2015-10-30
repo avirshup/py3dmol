8fefd56ab36ccaf47aea15433e444946ef5d8a6e
Aaron Virshup <aaron.virshup@autodesk.com>
2015-10-30 15:23:41 -0700

    Add pypi setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Py3DMol',
+    version='0.1',
+    packages=['py3dmol'],
+    url='http://github.com/avirshup',
+    license='Simplified BSD',
+    author='Aaron Virshup',
+    author_email='',
+    description='Molecular visualization tools for the iPython notebook based on 3DMol.js'
+)
